8acd9c9e53c2990818d63b8a49fc32684aabfe0d
Ahn-Soo-Min <vine-trawl-0b@icloud.com>
2023-11-08 16:41:45 +0900

    Second Commit

--- AlarmClock.js
+++ AlarmClock.js
@@ -1,14 +1,28 @@
-const clock = document.querySelector(".clock");
-const today = document.querySelector(".today");
+function setClock(){
+  var dateInfo = new Date();
+  
+  var hour = modifyNumber(dateInfo.getHours());
+  var min = modifyNumber(dateInfo.getMinutes());
+  var sec = modifyNumber(dateInfo.getSeconds());
 
-function getClock(){
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+  var year = dateInfo.getFullYear();
+  var month = dateInfo.getMonth() + 1;
+  var date = dateInfo.getDate();
 
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
+  document.getElementById("date").innerHTML = year + "년" + month + "월" + date + "일";
 }
 
-getClock();
-setInterval(getClock, 1000);
+function modifyNumber(time) {
+  if(parseInt(time) < 10){
+    return "0" + time;
+  }
+  else{
+    return time;
+  }
+}
+
+window.onload =function(){
+  setClock();
+  setInterval(setClock, 1000);
+}
